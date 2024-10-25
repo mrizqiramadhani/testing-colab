7a5fee60ef472114af1364529068791ddcd2d6c1
Muhammad Rizqi Ramadhani <m.rizqiramadhani02@gmail.com>
2024-10-25 09:52:38 +0800

    update branch lacies

---
+++ resources/views/tabrak.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing nabrak</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis veritatis ad modi voluptatibus libero voluptatem nesciunt laboriosam cupiditate animi maxime repellendus facere, aliquam optio velit reprehenderit explicabo tempore culpa totam!</p>
+</body>
+</html>
