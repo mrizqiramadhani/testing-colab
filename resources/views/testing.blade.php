169959776677cef230b79adda5c6b41ad91b346e
Muhammad Rizqi Ramadhani <m.rizqiramadhani02@gmail.com>
2024-10-25 09:23:30 +0800

    testing

---
+++ resources/views/testing.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero rerum eligendi doloremque, neque quia itaque porro dolore expedita delectus praesentium sint quisquam saepe, ea quaerat fugiat repellat, quas provident architecto!</p>
+</body>
+</html>
